8b07623a8c7c40a31abd67250f0f1cab57c347f5
ale.massari <105865898+alessandromassari@users.noreply.github.com>
2025-05-26 17:08:35 +0200

    Update utilities.py new ver corrected path

--- source/utilities.py
+++ source/utilities.py
@@ -8,12 +8,20 @@ def add_zeros(data):
     return data
     
 def create_dirs():
-    directories = ['checkpoints','logs']
-    for directory in directories:
-        os.makedirs(directory, exist_ok= True)
+    actual_dir = os.path.dirname(os.path.abspath(__file__))
+    project_dir = os.path.dirname(actual_dir)
+
+    # create directories 
+    checkpoints_dir = os.path.join(project_dir, 'checkpoints')
+    logs_dir = os.path.join(project_dir, 'logs')
+    os.makedirs(checkpoints_dir, exist_ok= True)
+    os.makedirs(logs_dir, exist_ok= True)
+
+    print(f"Logs and checkpoints directories created.")
+
 # LOGGING FORSE DA IMPLEMENTARE DIRETTAMENTE IN MAIN TRAMITE LOGGING LIB
 #def set_logging(model, test_dir_name, ep: int)
-#    filename = f"logs/model_{test_dir_name}_epoch_{ep}.pth"
+#   filename = f"logs/model_{test_dir_name}_epoch_{ep}.pth"
 #   print(f"Checkpoint saved to {filename}")
 
                 
