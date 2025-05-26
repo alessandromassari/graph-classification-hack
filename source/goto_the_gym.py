2a5536f86e0a27d1342e58cc80b4d134a0901fe4
ale.massari <105865898+alessandromassari@users.noreply.github.com>
2025-05-26 22:48:18 +0200

    Update goto_the_gym.py aaargh an_ep_kl

--- source/goto_the_gym.py
+++ source/goto_the_gym.py
@@ -75,7 +75,7 @@ def train(model, td_loader, optimizer, device, kl_weight_max, cur_epoch, an_ep_k
     total_loss = 0.0
 
     # compute dynamic KL weight
-    if cur_epoch < annealing_epoch:
+    if cur_epoch < an_ep_kl:
         kl_weight = kl_weight_max * (cur_epoch / an_ep_kl)
     else:
         kl_weight = kl_weight_max
