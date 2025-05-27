2297e2f5b9fd1250eb8261ba91bec3b7e5c2c402
ale.massari <105865898+alessandromassari@users.noreply.github.com>
2025-05-27 02:24:11 +0200

    Update goto_the_gym.py new release

--- source/goto_the_gym.py
+++ source/goto_the_gym.py
@@ -24,12 +24,10 @@ def eval_reconstruction_loss(adj_pred, edge_index, num_nodes, num_neg_samp=1):
     negative_logits = adj_pred[neg_edge_index[0],neg_edge_index[1]]
     negative_labels = torch.zeros_like(negative_logits)
 
-    # DEBUG info: use .cat instead of np.concatenate() to keep all on GPU
     all_the_logits = torch.cat([positive_logits, negative_logits])
     all_the_labels = torch.cat([positive_labels, negative_labels])
 
     recon_loss = F.binary_cross_entropy(all_the_logits,all_the_labels)
-
     return recon_loss
 
 # Pre training procedure - no classifiers here
@@ -49,31 +47,36 @@ def pretraining(model, td_loader, optimizer, device, kl_weight_max, cur_epoch, a
         data = data.to(device)
         # reset the gradients each batch 
         optimizer.zero_grad()
-        
-        adj_pred,mu,logvar,class_logits = model(data.x,
-                                                data.edge_index,
-                                                data.batch,
-                                                enable_classifier=False) 
-        
+
+        # new release update: pass whole data instead of .x ecc.
+        adj_pred,mu,logvar,class_logits, z = model(data, enable_classifier=False) 
+
+        # just a check
+        if adj_pred is None or mu is None or logvar is None:
+            print(f"Warning: Model output is None in pretraining epoch {cur_epoch+1}. Skip the batch")
+            continue
+            
         #KL term loss
         kl_term_loss = kl_loss(mu, logvar)
         #reconstruction loss 
-        reconstruction_loss = eval_reconstruction_loss(adj_pred,data.edge_index,data.x.size(0),num_neg_samp=1)
-        #total loss
+        reconstruction_loss = eval_reconstruction_loss(adj_pred, data.edge_index, data.x.size(0), num_neg_samp=1)
+        #total pretraining loss
         loss = kl_weight*kl_term_loss + reconstruction_loss
         loss.backward()
         optimizer.step()
 
         # accumulate total losses
-        total_loss += loss.item() * data.num_graphs if hasattr(data, 'num_graphs') else loss.item()#weight per pesare
+        total_loss += loss.item() #* data.num_graphs if hasattr(data, 'num_graphs') else loss.item()#weight per pesare
 
     return total_loss/len(td_loader)
     
-# Training procedure
+# Training procedure - classifier is in!
 def train(model, td_loader, optimizer, device, kl_weight_max, cur_epoch, an_ep_kl):
     model.train()
     total_loss = 0.0
-
+    total_guessed_pred = 0
+    total_worked_graph = 0
+    
     # compute dynamic KL weight
     if cur_epoch < an_ep_kl:
         kl_weight = kl_weight_max * (cur_epoch / an_ep_kl)
@@ -87,20 +90,37 @@ def train(model, td_loader, optimizer, device, kl_weight_max, cur_epoch, an_ep_k
         # reset the gradients each batch 
         optimizer.zero_grad()
         
-        adj_pred, mu, logvar, class_logits = model(data.x, data.edge_index, data.batch)
-     
+        adj_pred, mu, logvar, class_logits = model(data, enable_classifier=True)
+
+        # just a check
+        if adj_pred is None or mu is None or logvar is None:
+            print(f"Warning: Model output is None in training epoch {cur_epoch+1}. Skip the batch")
+            continue
+            
         #classification loss
-        classification_loss = F.cross_entropy(class_logits, data.y)
+        #DEBUG instead of just data.y
+        if data.y.dim() > 1 and data.y.size(1) == 1:
+            target_y = data.y.squeeze(1)
+        else:
+            target_y = data.y
+        classification_loss = F.cross_entropy(class_logits, target.y) 
+        
         #KL term loss
         kl_term_loss = kl_loss(mu, logvar)
         #reconstruction loss 
-        reconstruction_loss = eval_reconstruction_loss(adj_pred,data.edge_index,data.x.size(0),num_neg_samp=1)
+        reconstruction_loss = eval_reconstruction_loss(adj_pred, data.edge_index, data.x.size(0), num_neg_samp=1)
         #total loss
         loss = classification_loss + kl_weight*kl_term_loss + reconstruction_loss
         loss.backward()
         optimizer.step()
 
         # accumulate total losses
-        total_loss += loss.item() * data.num_graphs if hasattr(data, 'num_graphs') else loss.item() # provare weight per pesare
-
-    return total_loss/len(td_loader)
+        total_loss += loss.item() #* data.num_graphs if hasattr(data, 'num_graphs') else loss.item() # provare weight per pesare
+        
+        #DEBUG evaluate accuracy 
+        preds = torch.argmax(class_logits, dim=1)
+        total_guessed_pred += (preds == target_y).sum().item()
+        total_worked_graphs += data.num_graphs 
+        ep_accuracy = total_guessed_pred / total_worked_graph
+        
+    return total_loss/len(td_loader), ep_accuracy
