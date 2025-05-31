83d3aee5a75b9506b810d716933438f2d1d444ff
ale.massari <105865898+alessandromassari@users.noreply.github.com>
2025-05-31 11:56:50 +0200

    Update goto_the_gym.py NEW RECON LOSS

--- source/goto_the_gym.py
+++ source/goto_the_gym.py
@@ -5,9 +5,11 @@ import torch.nn.functional as F
 from torch_geometric.utils import to_dense_adj, negative_sampling
 from my_model import VGAE_all
 from torch.nn import SmoothL1Loss
-from losses import FocalLoss
+from losses import FocalLoss,  compute_recon_loss
+
+
 # reconstruction loss weight - DA SPOSTARE DA QUI
-recon_weight = 0.2 # previous value: 0.8
+recon_weight = 0.1     # previous value: 0.2
 focal = FocalLoss(gamma=2.0)
 
 # our beloved Kullback-Leibler term loss
@@ -33,11 +35,12 @@ def eval_reconstruction_loss(adj_pred, edge_index, num_nodes, num_neg_samp=1):
     all_the_logits = torch.cat([positive_logits, negative_logits])
     all_the_labels = torch.cat([positive_labels, negative_labels])
 
-    recon_loss = F.binary_cross_entropy(all_the_logits,all_the_labels)
-    return recon_loss
+# PROX FUNZIONI DI LOSS DA CANCELLARE 
+   # recon_loss = F.binary_cross_entropy(all_the_logits,all_the_labels)
+   # return recon_loss
 
 # a new loss: huber loss instead of reconstruction loss - FORSE DA CANCELLARE 
-def reconstruction_huber_loss(z, edge_index, model_decoder, num_nodes, num_neg_samp=1, beta=1.0):
+"""def reconstruction_huber_loss(z, edge_index, model_decoder, num_nodes, num_neg_samp=1, beta=1.0):
     """
     Ricostruzione edge-wise con Huber loss tra z e all_edges (positivi + negativi).
     """
@@ -63,7 +66,7 @@ def reconstruction_huber_loss(z, edge_index, model_decoder, num_nodes, num_neg_s
     # Huber loss
     huber = SmoothL1Loss(beta=beta)
     return huber(adj_pred, all_labels)
-    
+    """
 # Pre training procedure - no classifiers here
 def pretraining(model, td_loader, optimizer, device, kl_weight_max, cur_epoch, an_ep_kl):
     model.train()
@@ -94,8 +97,9 @@ def pretraining(model, td_loader, optimizer, device, kl_weight_max, cur_epoch, a
         kl_term_loss = kl_loss(mu, logvar)
         #reconstruction loss 
         #reconstruction_loss = eval_reconstruction_loss(adj_pred, data.edge_index, data.x.size(0), num_neg_samp=1)
-        reconstruction_loss = reconstruction_huber_loss(z=z,edge_index=data.edge_index,model_decoder=model.decoder,num_nodes=data.x.size(0),num_neg_samp=1,beta=1.0)
-        
+        #reconstruction_loss = reconstruction_huber_loss(z=z,edge_index=data.edge_index,model_decoder=model.decoder,num_nodes=data.x.size(0),num_neg_samp=1,beta=1.0)
+        reconstruction_loss = compute_recon_loss(z, data.edge_index, data.edge_attr)
+
         #total pretraining loss
         loss = kl_weight*kl_term_loss + reconstruction_loss
         loss.backward()
@@ -125,8 +129,8 @@ def train(model, td_loader, optimizer, device, kl_weight_max, cur_epoch, an_ep_k
         data = data.to(device)
         # reset the gradients each batch 
         optimizer.zero_grad()
-        
-        adj_pred, mu, logvar, class_logits, z = model(data, enable_classifier=True)
+
+        adj_pred, edge_attr_pred, mu, logvar, class_logits, z = model(data, enable_classifier=True)
 
         # just a check
         if adj_pred is None or mu is None or logvar is None:
@@ -146,8 +150,9 @@ def train(model, td_loader, optimizer, device, kl_weight_max, cur_epoch, an_ep_k
         #kl_term_loss = kl_loss(mu, logvar)
         #reconstruction loss 
         #reconstruction_loss = eval_reconstruction_loss(adj_pred, data.edge_index, data.x.size(0), num_neg_samp=1)
-        reconstruction_loss = reconstruction_huber_loss(z=z,edge_index=data.edge_index,model_decoder=model.decoder,num_nodes=data.x.size(0),num_neg_samp=1,beta=1.0)
-        
+        #reconstruction_loss = reconstruction_huber_loss(z=z,edge_index=data.edge_index,model_decoder=model.decoder,num_nodes=data.x.size(0),num_neg_samp=1,beta=1.0)
+        reconstruction_loss = compute_recon_loss(z, data.edge_index, data.edge_attr)
+
         #total loss
         loss = classification_loss + recon_weight*reconstruction_loss
         loss.backward()
